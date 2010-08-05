6b34364ad97138b7cb6bbe6051b9d776b43ada82
Anton Povarov <anton.povarov@gmail.com>
2010-08-05 18:43:33 +0400

    added hash from cpp-util added utility/offsetof.hpp for offsetof() that doesn't give gcc warnings

---
+++ meow/utility/offsetof.hpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+// vim: set autoindent noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker :
+// Copyright(c) 2009+ Anton Povarov <anton.povarov@gmail.com>
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MEOW_UTILITY__OFFSETOF_HPP_
+#define MEOW_UTILITY__OFFSETOF_HPP_
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// simple macros for member offsets and arithmetic utils
+
+#define MEOW_OFFSETOF(class_name, member_name) \
+	ptrdiff_t(((char*)&((class_name*)1)->member_name) - 1)
+
+#define MEOW_SELF_FROM_MEMBER(class_name, member_name, member_ptr) \
+	((class_name*)((char*)member_ptr - MEOW_OFFSETOF(class_name, member_name)))
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // MEOW_UTILITY__OFFSETOF_HPP_
+
