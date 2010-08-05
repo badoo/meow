a5590f3f0dcc5bfddbef08b1166f4ff99379e7be
Anton Povarov <anton.povarov@gmail.com>
2010-08-05 20:05:56 +0400

    added quick c++-like string->number caster, descendent of util::lex_cast<>

---
+++ meow/convert/number_from_string.hpp
@@ -0,0 +1,106 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+// vim: set filetype=cpp autoindent noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker :
+// (c) 2010 Anton Povarov <anton.povarov@gmail.com>
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MEOW_CONVERT__NUMBER_FROM_STRING_HPP_
+#define MEOW_CONVERT__NUMBER_FROM_STRING_HPP_
+
+#include <exception>
+
+#include <boost/utility/enable_if.hpp>
+
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/type_traits/is_signed.hpp>
+#include <boost/type_traits/is_unsigned.hpp>
+
+#include <meow/str_ref.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+namespace meow {
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+	struct bad_number_from_string : public std::bad_cast
+	{
+		virtual char const* what() const throw()
+		{
+			return "number_from_string: can't cast source to target arithmetic type";
+		}
+	};
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+namespace detail {
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+	template<class T, class Enabler = void>
+	struct number_from_string_caster_t;
+
+	template<class T>
+	struct number_from_string_caster_t<
+			  T
+			, typename boost::enable_if_c<
+				   boost::is_integral<T>::value
+				&& (sizeof(T) <= sizeof(long))
+			>::type
+			>
+	{
+		static T cast(char const *b, char **e, typename boost::enable_if<boost::is_signed<T> >::type * = 0)
+		{
+			return ::strtol(b, e, 0);
+		}
+
+		static T cast(char const *b, char **e, typename boost::enable_if<boost::is_unsigned<T> >::type * = 0)
+		{
+			return ::strtoul(b, e, 0);
+		}
+	};
+
+#define DEFINE_SIMPLE_NUMBER_CASTER(type, function) 	\
+	template<> 											\
+	struct number_from_string_caster_t<type> { 			\
+		static type cast(char const *b, char **e) { return function(b, e, 0); } 	\
+	}; 													\
+/**/
+
+#define DEFINE_SIMPLE_FLOATING_CASTER(type, function) 	\
+	template<> 											\
+	struct number_from_string_caster_t<type> { 			\
+		static type cast(char const *b, char **e) { return function(b, e); } 	\
+	}; 													\
+/**/
+
+	DEFINE_SIMPLE_NUMBER_CASTER(long long, 			::strtoll);
+	DEFINE_SIMPLE_NUMBER_CASTER(unsigned long long, ::strtoull);
+	DEFINE_SIMPLE_FLOATING_CASTER(float, 			::strtod);
+	DEFINE_SIMPLE_FLOATING_CASTER(double, 			::strtod);
+	DEFINE_SIMPLE_FLOATING_CASTER(long double, 		::strtold);
+
+#undef DEFINE_SIMPLE_FLOATING_CASTER
+#undef DEFINE_SIMPLE_NUMBER_CASTER
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+} // namespace detail {
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+	template<class To>
+	To number_from_string(char const *b, char const *e)
+	{
+		char *p = e;
+		To const to = detail::number_from_string_caster_t<To>::cast(b, &p);
+		if (p != e)
+			throw bad_number_from_string();
+		return to;
+	}
+
+	template<class To>
+	To number_from_string(str_ref const& s)
+	{
+		return number_from_string<To>(s.begin(), s.end());
+	}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+} // namespace meow {
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // MEOW_CONVERT__NUMBER_FROM_STRING_HPP_
+
