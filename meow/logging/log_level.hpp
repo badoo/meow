83a6403061297b22fa4e7c58848f886361153533
Anton Povarov <anton.povarov@gmail.com>
2010-08-10 19:07:49 +0400

    added detailed log level descriptions, ninja'd from da internetz

--- meow/logging/log_level.hpp
+++ meow/logging/log_level.hpp
@@ -13,16 +13,56 @@ namespace meow { namespace logging {
 	// ninja'd from syslog levels
 	MEOW_DEFINE_SMART_ENUM(
 			log_level,
+
+			/* The most severe messages that prevent continuation of operation,
+			 *  such as immediate system shutdown.
+			 *  PANIC OMG OMG, KILL ALL HUMANS...
+			 */
 			((emerg, 		"emerg"))
+
+			/* System conditions requiring immediate attention
+			 * (for example 
+			 * 	corrupted system database,
+			 * 	insufficient disk space,
+			 * 	run out of file descriptors, etc)
+			 */
 			((alert, 		"alert"))
+
+			/* Mostly serious system/application malfunctioning,
+			 *  such as failing hardware (hard device errors) or software.
+			 *  Usually non-recoverable.
+			 */
 			((crit, 		"crit"))
+
+			/* Mostly correctable errors, for example errors other that hard device errors.
+			 * Continuation of the operation is possible.
+			 * Usually all err conditions are automatically recoverable.
+			 */
 			((error, 		"error"))
+
+			/* Warning messages, we pretend to care.
+			 * High probability of turning into 'error' or 'crit' if not attended soon enough
+			 * 	(for example: free disk space at 5%, memory low, etc.)
+			 */
 			((warn, 		"warn"))
+
+			/* Notices requiring attention at a later time.
+			 * Non-error conditions that might require special handling.
+			 * Difference with the warning is not very clear.
+			 * We pretend to care... again.
+			 */
 			((notice, 		"notice"))
+
+			/* Informational messages */
 			((info, 		"info" ))
+
+			/* Messages for debugging purposes. */
 			((debug, 		"debug"))
+
+			/* turned off, writing nothing */
 			((off, 			"off"))
-			);
+
+		);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 }} // namespace meow { namespace logging {
