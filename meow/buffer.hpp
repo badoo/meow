20a1f96fe49ab6eacb11a3698ced1be021e9da95
Anton Povarov <anton.povarov@gmail.com>
2010-09-15 16:38:56 +0400

    added clear() function

--- meow/buffer.hpp
+++ meow/buffer.hpp
@@ -100,6 +100,8 @@ namespace meow {
 		bool is_full() const { return (last == end_); }
 		bool is_empty() const { return (first == last); }
 
+		void clear() { first = last = begin_; invariant_check(); }
+
 		void reset_first(char_t *p) { first = p; invariant_check(); }
 		void reset_last(char_t *p) { last = p; invariant_check(); }
 
