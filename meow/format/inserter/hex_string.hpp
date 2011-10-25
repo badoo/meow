3d6ff9191b17475ed979479a7dbc428be53266e3
Anton Povarov <anton.povarov@gmail.com>
2011-10-25 17:17:52 +0400

    format/hex_string fixes, simplified it quite a bit

--- meow/format/inserter/hex_string.hpp
+++ meow/format/inserter/hex_string.hpp
@@ -8,8 +8,6 @@
 
 #include <string>
 
-#include <boost/range/iterator_range.hpp>
-
 #include <meow/str_ref.hpp>
 #include <meow/convert/hex_to_from_bin.hpp>
 #include <meow/format/metafunctions.hpp>
@@ -19,68 +17,26 @@ namespace meow { namespace format {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 	template<class CharT>
-	struct hex_string_wrapper_t
+	std::basic_string<CharT> as_hex_string(string_ref<CharT> const& s)
 	{
-		string_ref<CharT const> str;
-	};
+		std::basic_string<CharT> result(s.size() * 2, CharT());
 
-	template<class CharT>
-	inline hex_string_wrapper_t<CharT> as_hex_string(string_ref<CharT> const& s)
-	{
-		hex_string_wrapper_t<CharT> r;
-		r.str = s;
-		return r;
+		CharT *ee = copy_bin2hex(s.begin(), s.end(), &*result.begin());
+		BOOST_ASSERT(ee == &*result.end());
+
+		return result;
 	}
 
 	template<class CharT>
-	struct type_tunnel<hex_string_wrapper_t<CharT> >
+	std::basic_string<CharT> as_escaped_hex_string(string_ref<CharT> const& s)
 	{
-		typedef typename boost::remove_const<CharT>::type string_char_t;
-		typedef std::basic_string<string_char_t> string_t;
+		std::basic_string<CharT> result(s.size() * 4, CharT());
 
-		static string_t call(hex_string_wrapper_t<CharT> const& s)
-		{
-			string_t result(s.str.size() * 2, string_char_t());
+		CharT *ee = copy_bin2hex_escaped(s.begin(), s.end(), &*result.begin());
+		BOOST_ASSERT(ee == &*result.end());
 
-			CharT *ee = copy_bin2hex(s.str.begin(), s.str.end(), &*result.begin());
-			BOOST_ASSERT(ee == &*result.end());
-
-			return result;
-		}
-	};
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-	template<class CharT>
-	struct hex_escaped_string_wrapper_t
-	{
-		string_ref<CharT const> str;
-	};
-
-	template<class CharT>
-	inline hex_escaped_string_wrapper_t<CharT> as_escaped_hex_string(string_ref<CharT> const& s)
-	{
-		hex_escaped_string_wrapper_t<CharT> r;
-		r.str = s;
-		return r;
+		return result;
 	}
-
-	template<class CharT>
-	struct type_tunnel<hex_escaped_string_wrapper_t<CharT> >
-	{
-		typedef typename boost::remove_const<CharT>::type string_char_t;
-		typedef std::basic_string<string_char_t> string_t;
-
-		static string_t call(hex_escaped_string_wrapper_t<CharT> const& s)
-		{
-			string_t result(s.str.size() * 4, string_char_t());
-
-			CharT *ee = copy_bin2hex_escaped(s.str.begin(), s.str.end(), &*result.begin());
-			BOOST_ASSERT(ee == &*result.end());
-
-			return result;
-		}
-	};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 }} // namespace meow { namespace format {
