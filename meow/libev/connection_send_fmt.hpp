11b01f55d7dcf107bd730fe006e1c000afba0050
Anton Povarov <anton.povarov@gmail.com>
2012-01-10 18:22:59 +0400

    libev: added connection_queue_fmt() that just queues the buffer without sending

--- meow/libev/connection_send_fmt.hpp
+++ meow/libev/connection_send_fmt.hpp
@@ -15,7 +15,7 @@ namespace meow { namespace libev {
 
 	#define DEFINE_CONNECTION_SEND_FMT_BODY(z, n, fn_name) 		\
 	template<class C, class F FMT_TEMPLATE_PARAMS(n)> 		\
-	inline void connection_send_fmt_l( 						\
+	inline void connection_queue_fmt_l( 					\
 			  C *c 											\
 			, size_t const init_sz 							\
 			, F const& fmt 									\
@@ -23,12 +23,18 @@ namespace meow { namespace libev {
 	{ 														\
 		buffer_move_ptr b(new buffer_t(init_sz)); 			\
 		format::fmt(*b, fmt FMT_CALL_SITE_ARGS(n)); 		\
-		c->send(move(b)); 									\
+		c->queue_buf(move(b)); 								\
+	} 														\
+	template<class C, class F FMT_TEMPLATE_PARAMS(n)> 		\
+	inline void connection_queue_fmt(C *c, F const& fmt FMT_DEF_PARAMS(n)) 	\
+	{ 														\
+		connection_queue_fmt_l(c, 1024, fmt FMT_CALL_SITE_ARGS(n)); 	\
 	} 														\
 	template<class C, class F FMT_TEMPLATE_PARAMS(n)> 		\
 	inline void connection_send_fmt(C *c, F const& fmt FMT_DEF_PARAMS(n)) 	\
 	{ 														\
-		connection_send_fmt_l(c, 1024, fmt FMT_CALL_SITE_ARGS(n)); 	\
+		connection_queue_fmt_l(c, 1024, fmt FMT_CALL_SITE_ARGS(n)); 	\
+		c->w_activate();									\
 	} 														\
 	/**/
 
