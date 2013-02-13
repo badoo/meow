97ff8ffd5adddf1073d7046235ee235b1a129850
Anton Povarov <anton.povarov@gmail.com>
2013-02-13 20:25:36 +0400

    added bind.hpp - wrapper for <functional> importing _1, _2 into global namespace

---
+++ meow/bind.hpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+// vim: set filetype=cpp autoindent noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker :
+// Copyright(c) Anton Povarov <anton.povarov@gmail.com>
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MEOW__BIND_HPP_
+#define MEOW__BIND_HPP_
+
+#include <functional>
+
+// import _1, _2 ... into global namespace like a boost::bind does
+using namespace std::placeholders;
+
+#endif // MEOW__BIND_HPP_
