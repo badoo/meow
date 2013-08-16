07b8d9e23d64564be0df1a1eac9e6f64ff4a10bf
Anton Povarov <anton.povarov@gmail.com>
2013-08-16 18:51:16 +0400

    minor fix

--- meow/std_unique_ptr.hpp
+++ meow/std_unique_ptr.hpp
@@ -19,12 +19,6 @@ namespace meow {
 	}
 
 	template<class T, class... A>
-	std::unique_ptr<T> unique_create(A&&... a)
-	{
-		return std::unique_ptr<T>(new T(std::forward<A>(a)...));
-	}
-
-	template<class T, class... A>
 	std::unique_ptr<T> make_unique(A&&... a)
 	{
 		return std::unique_ptr<T>(new T(std::forward<A>(a)...));
